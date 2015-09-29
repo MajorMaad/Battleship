80a385ebcd1ef320631dd7cc8fa8b4e378bc5cf2
MajorMaad <quentin.audinot@gmail.com>
2015-09-29 15:54:01 -0400

    Début du projet, ajout des classe Bateau, Battleship, Case et Grille + constructeur de la classe Bateau

---
+++ src/Bateau.java
@@ -0,0 +1,42 @@
+
+public class Bateau {
+	
+	private int longueur;
+	private int champ_tir;
+	private Case case_origine;
+	private String orientation;
+	private char symbole;
+
+	public Bateau(int longueur, int champ_tir, Case case_origine, String orientation, char symbole) {
+		
+		if (longueur > 1 && longueur <= 5)
+			this.longueur = longueur;
+		
+		if (champ_tir > 1 && champ_tir <= 5)
+			this.champ_tir = champ_tir;
+		
+		if (orientation == "nord" || orientation == "sud" || orientation == "est" || orientation == "ouest")
+			this.orientation = orientation;
+		
+		if (symbole == 'p' || symbole == 'c' || symbole == 'r' || symbole == 's' || symbole == 't')
+			this.symbole = symbole;
+
+		this.case_origine = case_origine;
+		
+	}
+	
+	/**
+	 * Méthode de déplacement du bateau
+	 * @param direction : direction du déplacement
+	 * @param nb : nombre de cases de déplacement
+	 */
+	public void deplacer(String direction, int nb) {
+		
+		if (orientation == "nord" || orientation == "sud" || orientation == "est" || orientation == "ouest") {
+			if (nb > 0 && nb <= 2) {
+				//this.case_origine.setEtat(' ');
+			}
+		}
+	}
+
+}
---
+++ src/Grille.java
@@ -0,0 +1,8 @@
+
+public class Grille {
+
+	public Grille() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
