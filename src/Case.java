e346a0df81ebb14d3020a6a833fa3227d6392057
MajorMaad <quentin.audinot@gmail.com>
2015-09-29 16:18:28 -0400

    Ajout de constructeurs pour la classe Case + la méthode toString

--- src/Case.java
+++ src/Case.java
@@ -1,8 +1,36 @@
 
 public class Case {
+	
+	private char ligne;
+	private char colonne;
+	private char etat;
 
-	public Case() {
-		// TODO Auto-generated constructor stub
+	public Case(char ligne, char colonne) {
+		if ("abcdefghij".indexOf(ligne) >= 0)
+			this.ligne = ligne;
+		
+		if ("0123456789".indexOf(colonne) >= 0)
+			this.colonne = colonne;
+		
+		this.etat = ' ';
+	}
+	
+	public Case(char ligne, char colonne, char etat) {
+		if ("abcdefghij".indexOf(ligne) >= 0)
+			this.ligne = ligne;
+		
+		if ("0123456789".indexOf(colonne) >= 0)
+			this.colonne = colonne;
+		
+		if (" pcrstox".indexOf(etat) >= 0)
+			this.etat = etat;
+	}
+	
+	public String toString() {
+		String str = String.valueOf(this.etat);
+		
+		return str;
 	}
 
 }
+	
