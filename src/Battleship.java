565539bedb2449096088ddea1041e033f5ad2f98
MajorMaad <quentin.audinot@gmail.com>
2015-09-29 18:40:52 -0400

    Test de l affichage de la grille dans le main

--- src/Battleship.java
+++ src/Battleship.java
@@ -2,8 +2,19 @@
 public class Battleship {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Grille grille = new Grille();
+		
+		// Porte Avion
+		Case origine = new Case('d', '2');
+		Bateau pa = new PorteAvion(origine, "sud");
+		boolean ok = grille.ajouterBateau(pa, 0);
+		
+		// Croiseur
+		Case origine2 = new Case('i', '0');
+		Bateau c = new Croiseur(origine2, "est");
+		boolean ok2 = grille.ajouterBateau(c, 1);
+		
+		System.out.println(grille.affichageAttaque());
 	}
 
 }
