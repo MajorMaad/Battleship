b8ce812005ce0abd1dd155b936c0d61ee5365d54
Marie <marie.darrigol@e.ujf-grenoble.fr>
2015-10-04 15:27:22 -0400

    L'IA peut orienter ses bateaux comme elle veut

--- src/JoueurIA.java
+++ src/JoueurIA.java
@@ -14,42 +14,25 @@ public class JoueurIA extends Joueur {
 	}
 	
 	/**
-	 * Méthode équivalente à celle du joueur physique, mais elle choisit une case automatiquement
-	 * Une ligne et une colonne sont choisies aléatoirement
-	 * L'orientation dépend dans quelle partie de la carte la case se trouve :
-	 * - quart haut-gauche : orientation Sud ou Est
-	 * - quart haut-droit : orientation Sud ou Ouest
-	 * - quart bas-gauche : orientation Nord ou Est
-	 * - quart bas-droit : orientation Nord ou Ouest
+	 * Méthode équivalente à celle du joueur physique, mais elle choisit une case automatiquement et aléatoirement
 	 * @return une string représentant les coordonnées (ligne, colonne) et l'orientation du bateau
 	 */
 	public String placerBateau(){
 		String infosCase = "";
 		
+		// Choix de la ligne
 		infosCase = infosCase + selectionAleatoire("abcdefghij");
+		// Choix de la colonne
 		infosCase = infosCase + selectionAleatoire("0123456789");
-		
-		// Choix de l'orientation du bateau
-		if("abcde".indexOf(infosCase.charAt(0)) >= 0){	// Moitié haute
-			if("01234".indexOf(infosCase.charAt(1)) >= 0){	// Moitié gauche
-				infosCase = infosCase + selectionAleatoire("se");
-			}else{	// Moitié droite
-				infosCase = infosCase + selectionAleatoire("so");
-			}
-		}else{	// Moitié basse
-			if("01234".indexOf(infosCase.charAt(1)) >= 0){	// Moitié gauche
-				infosCase = infosCase + selectionAleatoire("ne");
-			}else{	// Moitié droite
-				infosCase = infosCase + selectionAleatoire("no");
-			}
-		}
+		// Choix de l'orientation
+		infosCase = infosCase + selectionAleatoire("nseo");
 		
 		return infosCase;
 	}
 	
 	/**
 	 * Sélectionne aléatoirement un caractère parmis ceux passés en paramètre
-	 * Utilisée pour choisir une ligne ou une colonne aléatoirement
+	 * Utilisée pour choisir une ligne, une colonne ou une orientation aléatoirement
 	 * @param s une chaîne de caractère représentant tous les éléments pouvant être tirés
 	 * @return un de ces éléments
 	 */
